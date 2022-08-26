5876d8c875915989bd7479ca57797d36ed8b61fb
Max Adulyanukosol <namax7@gmail.com>
2022-08-25 21:47:35 -0400

    Initial commit

---
+++ models/omop/person.sql
@@ -0,0 +1,33 @@
+SELECT
+    ROW_NUMBER()OVER(ORDER BY p.id) AS person_id,
+    CASE upper(p.gender)
+        WHEN 'M' THEN 8507
+        WHEN 'F' THEN 8532
+    END AS gender_concept_id,
+    date_part('year', p.birthdate) AS year_of_birth,
+    date_part('month', p.birthdate) AS month_of_birth,
+    date_part('day', p.birthdate) AS day_of_birth,
+    p.birthdate AS birth_datetime,
+    CASE upper(p.race)
+        WHEN 'WHITE' THEN 8527
+        WHEN 'BLACK' THEN 8516
+        WHEN 'ASIAN' THEN 8515
+        ELSE 0
+    END AS race_concept_id,
+    CASE
+        WHEN upper(p.ethnicity) = 'HISPANIC' THEN 38003563
+        WHEN upper(p.ethnicity) = 'NONHISPANIC' THEN 38003564
+        ELSE 0
+    END AS ethnicity_concept_id,
+    NULL AS location_id,
+    NULL AS provider_id,
+    NULL AS care_site_id,
+    p.id AS person_source_value,
+    p.gender AS gender_source_value,
+    0 AS gender_source_concept_id,
+    p.race AS race_source_value,
+    0 AS race_source_concept_id,
+    p.ethnicity AS ethnicity_source_value,
+    0 AS ethnicity_source_concept_id
+FROM {{ source('synthea', 'patients') }} AS p
+WHERE p.gender IS NOT NULL
